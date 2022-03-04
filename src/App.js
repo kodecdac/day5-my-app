4ae8adf521d8daffb1de11ee81a1cc4643a17af6
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-04 10:30:10 +0530

    stateful list.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 function App() {
-  // STATIC LIST
-  let list = ["Delhi", "Caluctta", "Chennai", "Mumbai", "Kolkata"];
+  let [list, setList] = useState(["Delhi"]);
+
+  const addNewItem = () => {
+    let newList = ["Mumbai", ...list];
+    setList(newList);
+  };
 
   return (
     <div>
       <h1 className="text-primary">Working with List</h1>
+
+      <input type="button" value="Add New Item" onClick={addNewItem} />
 
       {list.map((item) => (
         <h1>{item}</h1>
