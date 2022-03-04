55aae863ad2a73cf34801fe4b4904a3b1c91886d
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2022-03-04 10:37:32 +0530

    tod list . ....

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ function App() {
   let [list, setList] = useState(["Delhi"]);
 
   const addNewItem = () => {
-    let newList = ["Mumbai", ...list];
+    let newItem = document.querySelector("#inputId").value;
+
+    let newList = [newItem, ...list];
     setList(newList);
+
+    document.querySelector("#inputId").value = "";
   };
 
   return (
     <div>
       <h1 className="text-primary">Working with List</h1>
 
+      <input type="text" id="inputId" />
       <input type="button" value="Add New Item" onClick={addNewItem} />
 
       {list.map((item) => (
-        <h1>{item}</h1>
+        <div>
+          <input type="checkbox" /> {item}
+        </div>
       ))}
     </div>
   );
