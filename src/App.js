8fa297fa1fab4574ee3b7ddb2454b7085e265e0d
santoshmondal <santosh.ece06@gmail.com>
2022-08-24 08:00:46 +0530

    temp1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import { useRef } from "react";
 
 function App() {
   const inputRef = useRef();
+  const [userName, setUserName] = useState("hello");
+
+  const handleUserName = (e) => {
+    console.log(e.target.value);
+    setUserName(e.target.value);
+  };
 
   const callMe = () => {
-    console.log("Hello World");
     console.log(inputRef.current.value);
+    console.log(userName);
   };
 
   return (
     <div>
       <h1>API INtegration</h1>
       <input type="text" ref={inputRef} />
+      <input type="text" value={userName} onChange={handleUserName} />
       <input type="button" value="Handle Click Event" onClick={callMe} />
     </div>
   );
